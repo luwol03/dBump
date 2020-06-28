166bf277bb2fec8eab14d32f112a2a55cfad9fc9
perclf <lrf@jhf.name>
2020-06-28 13:10:56 +0200

    Initial commit

---
+++ main.js
@@ -0,0 +1,24 @@
+const Discord = require("discord.js");
+const bot = new Discord.Client(); 
+
+const settings = require('./settings.json');
+bot.on('ready', () => {
+	console.log(`Logged in as ${bot.user.tag}`);
+});
+
+bot.on('message', (msg) => {
+	if(msg.author.bot) return;
+	if(!msg.content.startsWith('!')) return;
+	let cmd = msg.content.substring(0, 7);
+	if(cmd != "!d bump") return;
+	if(Date.now() - bot.lastBump <= 7200000) return msg.channel.send("Du kannst nur alle 120 Minuten bumpen.");
+	let embed = new Discord.MessageEmbed()
+	    .setTitle("Unleashed Design: Der beste Webdesign Discordserver")
+	    .setURL("https://unleashed-design.de")
+	    .setTimestamp()
+	    .setDescription(`${msg.author.toString()}\nBump erfolgreich👍`);
+	bot.lastBump = Date.now();
+	msg.channel.send(embed);
+});
+	
+bot.login(settings.token);
